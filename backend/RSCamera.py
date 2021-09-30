97ee510d68c06fa1d33a9dcfe1343a62da7dc1e4
jako <jako.aimsalu@gmail.com>
2021-09-30 20:45:31 +0300

    update

---
+++ backend/RSCamera.py
@@ -0,0 +1,65 @@
+import numpy as np
+
+import cv2
+import pyrealsense2 as rs
+
+from threading import Thread
+
+class RSCamera(object):
+    def __init__(self):
+        # configure depth and color streams
+        self.pipeline = rs.pipeline()
+        self.config = rs.config()
+
+        # Get device product line for setting a supporting resolution
+        self.pipeline_wrapper = rs.pipeline_wrapper(self.pipeline)
+        self.pipeline_profile = self.config.resolve(self.pipeline_wrapper)
+        self.device = self.pipeline_profile.get_device()
+        self.device_product_line = str(self.device.get_info(rs.camera_info.product_line))
+
+        # TODO: configure desired shit
+        # enable depth
+        # 848x480 90 fps max
+        # go 60
+        self.config.enable_stream(rs.stream.depth, 848, 480, rs.format.z16, 60)
+        # enable rgb
+        # 960x540 60 fps max, 1920x1080 30 possible
+        self.config.enable_stream(rs.stream.color, 960, 540, rs.format.bgr8, 60)
+
+        print("starting rs pipeline")
+        self.pipeline.start(self.config)
+        
+        # should be tuple?
+        self.color_frame = None
+        self.depth_frame = None
+
+        # start capture thread
+        self.thread = Thread(target=self.process_frame_thread)
+        self.thread.start()
+
+    def __del__(self):
+        print("stopping rs pipeline")
+        self.pipeline.stop()
+
+    def is_ready(self):
+        return self.color_frame
+
+    def process_frame_thread(self):
+        while True:
+            # wait for a coherent pair of frames: depth and color
+            frames = self.pipeline.wait_for_frames()
+            depth_frame = frames.get_depth_frame()
+            color_frame = frames.get_color_frame()
+            if not depth_frame or not color_frame:
+                continue # fuck
+
+            depth_image = np.asanyarray(depth_frame.get_data())
+            color_image = np.asanyarray(color_frame.get_data())
+
+            #depth_colormap = cv2.applyColorMap(cv2.convertScaleAbs(depth_image, alpha=0.03), cv2.COLORMAP_JET)
+
+            # this is retarded, we're going to handle depth and color differently anyway
+            #images = [(cv2.imencode(".jpg", x)[1]).tobytes() for x in (depth_frame, color_frame)]
+
+            self.depth_frame = cv2.imencode(".jpg", depth_image)[1].tobytes()
+            self.color_frame = cv2.imencode(".jpg", color_image)[1].tobytes()
