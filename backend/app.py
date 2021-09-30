9b999d833d7b2d28bd4b82b9d6c0d50bc3837f23
jako <jako.aimsalu@gmail.com>
2021-09-30 19:42:06 +0300

    support multiple feed service, add depth

--- backend/app.py
+++ backend/app.py
@@ -1,3 +1,4 @@
+import threading
 from flask import Flask, render_template, request, Response
 
 import time
@@ -11,7 +12,7 @@ from threading import Thread
 
 class RSCamera(object):
     def __init__(self):
-        # Configure depth and color streams
+        # configure depth and color streams
         self.pipeline = rs.pipeline()
         self.config = rs.config()
 
@@ -21,74 +22,79 @@ class RSCamera(object):
         self.device = self.pipeline_profile.get_device()
         self.device_product_line = str(self.device.get_info(rs.camera_info.product_line))
 
-        found_rgb = False
-        for s in self.device.sensors:
-            if s.get_info(rs.camera_info.name) == "RGB Camera":
-                found_rgb = True
-                break
-        if not found_rgb:
-            print("The demo requires Depth camera with Color sensor")
-            exit(0)
+        # TODO: configure desired shit
+        # enable depth
+        # 848x480 90 fps max
+        # go 60
+        self.config.enable_stream(rs.stream.depth, 848, 480, rs.format.z16, 60)
+        # enable rgb
+        # 960x540 60 fps max, 1920x1080 30 possible
+        self.config.enable_stream(rs.stream.color, 960, 540, rs.format.bgr8, 60)
 
-        self.config.enable_stream(rs.stream.depth, 640, 480, rs.format.z16, 30)
-
-        if self.device_product_line == "L500":
-            self.config.enable_stream(rs.stream.color, 960, 540, rs.format.bgr8, 30)
-        else:
-            self.config.enable_stream(rs.stream.color, 640, 480, rs.format.bgr8, 30)
-
-        # Start streaming
+        print("starting rs pipeline")
         self.pipeline.start(self.config)
+        
+        # should be tuple?
+        self.color_frame = None
+        self.depth_frame = None
+
+        # start capture thread
         self.thread = Thread(target=self.process_frame_thread)
         self.thread.start()
-
-        self.rgb_frame = None
 
     def __del__(self):
         print("stopping rs pipeline")
         self.pipeline.stop()
 
+    def is_ready(self):
+        return self.color_frame
+
     def process_frame_thread(self):
         while True:
-            # Wait for a coherent pair of frames: depth and color
+            # wait for a coherent pair of frames: depth and color
             frames = self.pipeline.wait_for_frames()
             depth_frame = frames.get_depth_frame()
             color_frame = frames.get_color_frame()
             if not depth_frame or not color_frame:
-                continue
+                continue # fuck
 
-            # Convert images to numpy arrays
             depth_image = np.asanyarray(depth_frame.get_data())
             color_image = np.asanyarray(color_frame.get_data())
 
-            # Apply colormap on depth image (image must be converted to 8-bit per pixel first)
-            depth_colormap = cv2.applyColorMap(cv2.convertScaleAbs(depth_image, alpha=0.03), cv2.COLORMAP_JET)
+            #depth_colormap = cv2.applyColorMap(cv2.convertScaleAbs(depth_image, alpha=0.03), cv2.COLORMAP_JET)
 
-            depth_colormap_dim = depth_colormap.shape
-            color_colormap_dim = color_image.shape
+            # this is retarded, we're going to handle depth and color differently anyway
+            #images = [(cv2.imencode(".jpg", x)[1]).tobytes() for x in (depth_frame, color_frame)]
 
-            # If depth and color resolutions are different, resize color image to match depth image for display
-            if depth_colormap_dim != color_colormap_dim:
-                resized_color_image = cv2.resize(color_image, dsize=(depth_colormap_dim[1], depth_colormap_dim[0]), interpolation=cv2.INTER_AREA)
-                images = np.hstack((resized_color_image, depth_colormap))
-            else:
-                images = np.hstack((color_image, depth_colormap))
-
-            ret, jpeg = cv2.imencode(".jpg", color_image)
-            self.rgb_frame = jpeg.tobytes()
+            self.depth_frame = cv2.imencode(".jpg", depth_image)[1].tobytes()
+            self.color_frame = cv2.imencode(".jpg", color_image)[1].tobytes()
 
 app = Flask(__name__)
 
 rs_cam = None
 
-# todo this serves current frame, too tired to debug
-def feed_generator():
-    while True:
-        yield (b"--frame\r\n"
-               b"Content-Type: image/jpeg\r\n\r\n" + rs_cam.rgb_frame + b"\r\n\r\n")
+@app.route("/depth-feed")
+def depth_feed():
+    def feed_generator():
+        while True:
+            yield (b"--frame\r\n"
+                b"Content-Type: image/jpeg\r\n\r\n" + rs_cam.depth_frame + b"\r\n\r\n")
+            time.sleep(1/25) # big sync fix
 
-@app.route("/rs")
-def video_feed():
+    while not rs_cam.is_ready():
+        time.sleep(0.2)
+    return Response(feed_generator(), mimetype="multipart/x-mixed-replace; boundary=frame")
+
+@app.route("/color-feed")
+def color_feed():
+    def feed_generator():
+        while True:
+            yield (b"--frame\r\n"
+                b"Content-Type: image/jpeg\r\n\r\n" + rs_cam.color_frame + b"\r\n\r\n")
+            time.sleep(1/25) # big sync fix
+
+    while not rs_cam.is_ready():
+        time.sleep(0.2)
     return Response(feed_generator(), mimetype="multipart/x-mixed-replace; boundary=frame")
 
 @app.route("/")
